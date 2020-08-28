961420815b3ea9975e972cc04b1a7b6a8d0a92d8
hellobrian <brianhan@hey.com>
2020-08-28 17:09:46 -0500

    complete and delete todos from home page

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ import styles from "./index.module.css";
 const fetcher = async (query) => await graphQLClient.request(query);
 
 export default function Home() {
-  const { data, error } = useSWR(
+  const { data, error, mutate } = useSWR(
     gql`
       {
         allTodos {
@@ -22,6 +22,46 @@ export default function Home() {
     `,
     fetcher
   );
+
+  const toggleTodo = async (id, completed) => {
+    const query = gql`
+      mutation PartialUpdateTodo($id: ID!, $completed: Boolean!) {
+        partialUpdateTodo(id: $id, data: { completed: $completed }) {
+          _id
+          completed
+        }
+      }
+    `;
+
+    const variables = {
+      id,
+      completed: !completed,
+    };
+
+    try {
+      await graphQLClient.request(query, variables);
+      mutate();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const deleteATodo = async (id) => {
+    const query = gql`
+      mutation DeleteATodo($id: ID!) {
+        deleteTodo(id: $id) {
+          _id
+        }
+      }
+    `;
+
+    try {
+      await graphQLClient.request(query, { id });
+      mutate();
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <Layout>
@@ -46,9 +86,27 @@ export default function Home() {
         <ul>
           {data.allTodos.data.map((todo) => (
             <li key={todo._id}>
+              <button
+                type="button"
+                onClick={() => toggleTodo(todo._id, todo.completed)}
+                style={{
+                  textDecorationLine: todo.completed ? "line-through" : "none",
+                }}
+              >
+                {todo.task}
+              </button>
+              {" | "}
               <Link href="/todo/[id]" as={`/todo/${todo._id}`}>
-                <a>{todo.task}</a>
+                <a>Edit</a>
               </Link>
+              {" | "}
+              <button
+                type="button"
+                onClick={() => deleteATodo(todo._id)}
+                style={{ background: "red", color: "white" }}
+              >
+                Delete
+              </button>
             </li>
           ))}
         </ul>
